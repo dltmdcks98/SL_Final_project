4b1953b233ee1683c4c8d376757c7190b3f89f1b
dltmdcks98 <dltmdcks98@gmail.com>
2022-10-31 18:01:49 +0900

    22.10.31 finalProject first commit

---
+++ src/main/java/com/slfinalproject/commurest/CommurestApplication.java
@@ -0,0 +1,13 @@
+package com.slfinalproject.commurest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommurestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommurestApplication.class, args);
+	}
+
+}
