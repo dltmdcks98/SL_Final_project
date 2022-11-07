424069e2b33171ac60b1366ee2f517a2a9899589
dltmdcks98 <dltmdcks98@gmail.com>
2022-11-07 15:50:38 +0900

    22.11.07 finalProject LSC HomeController생성 및 테스트 완

--- src/main/java/com/slfinalproject/commurest/controller/testController.java
+++ src/main/java/com/slfinalproject/commurest/controller/testController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class testController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test() {
+        System.out.println("test");
         return "test";
     }
 
