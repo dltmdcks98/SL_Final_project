f64fd5ed0dca4bb1ed681fd8f3e4cb6ee80a4542
lilutily <tjdxodnd225@gmail.com>
2022-11-07 16:40:10 +0900

    SQL 테이블 게시판, 댓글, 태그 추가

---
+++ src/main/resources/SQL/tag.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tag (
+     tag_id INT(10),
+     tag_value VARCHAR(300),
+     CONSTRAINT pk_tag PRIMARY KEY (tag_id)
+);
+
+DROP TABLE tag;
--- src/main/resources/SQL/user_account.sql
+++ src/main/resources/SQL/user_account.sql
@@ -11,40 +11,8 @@ CREATE TABLE user_account(
     );
 DROP TABLE user_account;
 
-CREATE TABLE board(
-    board_id INT(10) AUTO_INCREMENT,
-    user_id INT(10) NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    content TEXT,
-    hit INT(10) DEFAULT 0,
-    reg_date DATETIME DEFAULT current_timestamp,
-    recommend INT(10) DEFAULT 0,
-    image_url VARCHAR(200),
-    tag_id VARCHAR(300),
-    CONSTRAINT pk_board PRIMARY KEY (board_id),
-    CONSTRAINT board FOREIGN KEY (user_id)
-    REFERENCES user_account (user_id)
-);
 
-DROP TABLE board;
 
-CREATE TABLE comment (
-    comment_id INT(10) AUTO_INCREMENT,
-    user_id INT(10) NOT NULL,
-    c_content TEXT,
-    reg_date DATETIME DEFAULT current_timestamp,
-    recommend INT(10) DEFAULT 0,
-    CONSTRAINT pk_comment PRIMARY KEY (comment_id),
-    CONSTRAINT comment  FOREIGN KEY (user_id)
-    REFERENCES user_account (user_id)
-);
 
-DROP TABLE comment;
 
-CREATE TABLE tag (
-    tag_id INT(10),
-    tag_value VARCHAR(300),
-    CONSTRAINT pk_tag PRIMARY KEY (tag_id)
-);
 
-DROP TABLE tag;
