aa158cd9a34cb68e4b5f28091b885f116eb028e6
dltmdcks98 <dltmdcks98@gmail.com>
2022-11-09 16:02:53 +0900

    22.11.09 finalProject LSC gallery googleAPI(google custom search) 크롤링 -이미지

--- src/main/java/com/slfinalproject/commurest/galleryBoard/controller/GalleryController.java
+++ src/main/java/com/slfinalproject/commurest/galleryBoard/controller/GalleryController.java
@@ -3,18 +3,9 @@ package com.slfinalproject.commurest.galleryBoard.controller;
 import com.slfinalproject.commurest.galleryBoard.service.GalleryBoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
@@ -24,9 +15,10 @@ public class GalleryController {
 
     private final GalleryBoardService galleryBoardService;
 
-    @GetMapping("/list")
+    @GetMapping("")
     public String galleryHome() {
         log.info("Controller(gallery) -" + getClass());
+
         return "gallery_board/gallery";
     }
 
--- src/main/java/com/slfinalproject/commurest/galleryBoard/service/GalleryBoardService.java
+++ src/main/java/com/slfinalproject/commurest/galleryBoard/service/GalleryBoardService.java
@@ -2,13 +2,10 @@ package com.slfinalproject.commurest.galleryBoard.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,16 +17,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GalleryBoardService {
 
-    public List<String> getImgUrl(String name) {
-        List<String> imageUrl = null;
+    public List<String> getImgUrl(String tag) {
+        String name=tag;
+
+        List<String> imageUrl = new ArrayList<>();
+
         try {
             Connection.Response res = Jsoup.connect(
-                    "https://www.googleapis.com/customsearch/v1?key=AIzaSyDR5HP1XN77UbnmTX9YFQowSvjW5Lz-YxU&cx=c0eb2a35954e0499f&imageType=face&q=+" + name).ignoreContentType(true).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36").execute();
+                    "https://www.googleapis.com/customsearch/v1?key=AIzaSyDR5HP1XN77UbnmTX9YFQowSvjW5Lz-YxU&cx=c0eb2a35954e0499f&imageType=face&searchType=image&imageSize=MEDIUM&q=" + name).ignoreContentType(true).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36").execute();
 
             JSONObject json = new JSONObject(res.body());
-            for (int i = 0; i < 10; i++) {
-                imageUrl.add(json.getJSONArray("items").getJSONObject(i).getJSONObject("pagemap").getJSONArray("cse_image").getJSONObject(0).getString("src"));
-                log.info(imageUrl);
+            JSONArray items = json.getJSONArray("items");
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject item = items.getJSONObject(i);
+                String url = item.getString("link");
+//                JSONObject pagemap = item.getJSONObject("pagemap");
+//                JSONArray metatags = pagemap.getJSONArray("metatags");
+//                JSONObject metaData = metatags.getJSONObject(0);
+//                String url = metaData.getString("og:image");
+                System.out.println("no : "+i +" url :"+ url);
+                imageUrl.add(url);
             }
 
         } catch (IOException e) {
