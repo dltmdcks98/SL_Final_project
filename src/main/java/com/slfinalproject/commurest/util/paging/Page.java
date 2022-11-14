0618349c30216246fff0df20a1e804d63c47f014
lilutily <tjdxodnd225@gmail.com>
2022-11-14 14:28:31 +0900

    Revert "[22.11.14 성태웅] board 페이징 테스트 및 처리 완료"
    This reverts commit 91bf0365e2d63c3202910595cc8a5f42f93a3907.

---
+++ src/main/java/com/slfinalproject/commurest/util/paging/Page.java
@@ -0,0 +1,39 @@
+package com.slfinalproject.commurest.util.paging;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
+
+public class Page {
+    private int pageNum; // 페이지 번호
+    private int amount; // 페이지당 나올 게시물 개수
+    public Page() {
+        this.pageNum=1;
+        this.amount=20;
+    }
+    public int getStart() {
+        return (pageNum-1)*amount;
+    }
+    public void setPageNum(int pageNum) {
+        if(pageNum <=0 || pageNum > Integer.MAX_VALUE) {
+            this.pageNum=1;
+            return;
+        }
+        this.pageNum=pageNum;
+    }
+    /*
+    public void setAmount(int amount) {
+        if(amount <20 || amount >100) {
+            this.amount=20;
+            return;
+        }
+        this.amount=amount;
+    }
+     */
+
+}
+
