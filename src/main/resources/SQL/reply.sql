19fdd7f65b6226d0fa7b9449328e05f25881c24c
dltmdcks98 <dltmdcks98@gmail.com>
2022-12-06 09:55:06 +0900

    12.06 LSC SQL문 수정

--- src/main/resources/SQL/reply.sql
+++ src/main/resources/SQL/reply.sql
@@ -1,20 +1,20 @@
-CREATE TABLE reply(
-    reply_no INT(10) AUTO_INCREMENT,
-    reply_text VARCHAR(1000) NOT NULL,
-    reply_writer VARCHAR(50) NOT NULL,
-    reply_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    board_no INT(10) NOT NULL,
-    user_account INT(10) NOT NULL,
-    CONSTRAINT pk_reply PRIMARY KEY (reply_no),
-    CONSTRAINT fk_reply_board FOREIGN KEY (board_no)
-                  REFERENCES board(board_no)
-                  ON DELETE CASCADE
-                  ON UPDATE CASCADE,
-    CONSTRAINT fk_reply_user FOREIGN KEY (user_account)
-                  REFERENCES user_account(user_id)
-                  ON DELETE CASCADE
-                  ON UPDATE CASCADE
+create table reply
+(
+    reply_no     int(10) auto_increment
+        primary key,
+    reply_text   varchar(1000)                        not null,
+    reply_writer varchar(50)                          not null,
+    reply_date   datetime default current_timestamp() null,
+    board_no     int(10)                              not null,
+    user_account int(10)                              not null,
+    constraint fk_reply_board
+        foreign key (board_no) references board (board_no)
+            on update cascade on delete cascade,
+    constraint fk_reply_user
+        foreign key (user_account) references user_account (user_id)
+            on update cascade on delete cascade
 );
+
 
 alter table reply
     change reply reply_no int(10) auto_increment;
